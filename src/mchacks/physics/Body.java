c0a80cdbd346a2d04ec6faae90524e8bd36ff08e
NicholasConnors <nicholasconnors@hotmail.ca>
2018-02-03 18:01:11 -0500

    changed body

--- src/mchacks/physics/Body.java
+++ src/mchacks/physics/Body.java
@@ -5,8 +5,9 @@ import mchacks.util.Vector;
 public class Body {	
 	private double mass; //kg 
 	private double radius; //m
-	private double period; 
-	private double speedRot;
+	
+	private double rotation;
+	private double rotationSpeed;
 	
 	public Vector pos;
 	public Vector vel;
@@ -22,21 +23,20 @@ public class Body {
 		
 		this.mass = 0.0;
 		this.radius = 0.0;
-		this.period = 0.0;
-		
-		this.speedRot = 0.0;
+
+		this.rotation = 0.0;
+		this.rotationSpeed = 0.0;
 	}
 	
-	public Body(double m, double r, double s){
+	public Body(double mass, double radius, double period){
 		this.pos = new Vector();
 		this.vel = new Vector();
 		this.acc = new Vector();
 		
-		this.mass = m;
-		this.radius = r;
-		this.period = s;
+		this.mass = mass;
+		this.radius = radius;
 		
-		this.speedRot = (2*Math.PI*radius)/period;
+		this.rotationSpeed = (2 * Math.PI * radius) / period;
 	}
 	
 	public double getMass () {
@@ -45,10 +45,6 @@ public class Body {
 	
 	public double getRadius () {
 		return this.radius;
-	}
-	
-	public double getPeriod () {
-		return this.period;
 	}
 	
 	public Vector getPos () {
@@ -60,10 +56,6 @@ public class Body {
 	
 	public double setRadius () {
 		return this.radius;
-	}
-	
-	public double gsetPeriod () {
-		return this.period;
 	}
 	
 	public Vector setPos () {
