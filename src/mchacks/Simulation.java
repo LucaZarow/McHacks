0f4613a80b3e710a5a2633d4a6fa40f5a19df14d
NicholasConnors <nicholasconnors@hotmail.ca>
2018-02-03 16:11:10 -0500

    set up simulation class

--- src/mchacks/Simulation.java
+++ src/mchacks/Simulation.java
@@ -1,15 +1,70 @@
 package mchacks;
 
-public class Simulation implements Runnable {
-
+public class Simulation {
+	private boolean running;
+	private long framesPerSecond, updatesPerSecond;
+	
+	private Thread renderThread = new RenderThread();
+	private Thread updateThread = new UpdateThread();
+	
 	public Simulation() {
 		
 	}
 	
+	public void start() {
+		running = true;
+		
+		updateThread.start();
+		renderThread.start();
+	}
 	
+	private void render() {
+		
+	}
 	
-	@Override
-	public void run() {
+	private void update(double dt) {
+		
+	}
+	
+	private class RenderThread extends Thread {
+		public synchronized void run() {
+			while(running) {
+				render();
+				framesPerSecond++;
+			}
+		}
+	}
+	
+	private class UpdateThread extends Thread {
+		public synchronized void run() {
+			long then = System.nanoTime();
 			
+			double timer = 0;
+			
+			while(running) {
+				long now = System.nanoTime();
+				double dt = (now - then) / 1000000000.0;
+				
+				timer += dt;
+				updatesPerSecond++;
+				
+				if(timer > 1) {
+					timer -= 1;
+					System.out.println("Updates: " + updatesPerSecond + "\tFPS: " + framesPerSecond);
+					
+					updatesPerSecond = 0;
+					framesPerSecond = 0;
+				}
+				update(dt);
+				
+				//Sleep
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				then = now;
+			}
+		}
 	}
 }
