ffea23d29a254d35636160360e56222c6715fdef
Luca Zarow <luca.zarow@mail.mcill.ca>
2018-02-03 18:15:16 -0500

    main

--- src/mchacks/Main.java
+++ src/mchacks/Main.java
@@ -1,8 +1,11 @@
 package mchacks;
 
+import mchacks.graphics.*;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		new GraphicsApp();
+		new Simulation().start();
 	}
 }
