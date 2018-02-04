eb0b10bc7a5ac61184e461090411bf26ee670079
ericgeoffrion <ericgeoffrion@142.157.8.204>
2018-02-03 22:46:43 -0500

    orthogonal

--- src/mchacks/physics/Physics.java
+++ src/mchacks/physics/Physics.java
@@ -18,4 +18,13 @@ public class Physics {
 		
 		return Vector.product(-deltaAcc, position.getUnitVector());
 	}
+	
+	public static double rocheLimit (Body b1, Body b2){
+		double b1Density = b1.getMass()/(4/3)*Math.PI*Math.pow(b1.getRadius(), 3);
+		double b2Density = b2.getMass()/(4/3)*Math.PI*Math.pow(b2.getRadius(), 3);
+		
+		double roche = 2*Math.pow(2,  1.333333)*b1.getRadius()*(b1Density/b2Density);	
+		
+		return roche;
+	}
 }
